538d912cfb8b0bb752bfa15eb0423cef879b327c
Yeongjin Kim <7591blitzloft@gmail.com>
2021-08-25 15:49:54 +0900

    Pybo project initial commit

---
+++ pybo/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from pybo.models import Question, Answer, Comment
+
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ['subject', 'content']
+        labels = {
+            'subject': 'Title',
+            'content': 'Content'
+        }
+
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content': 'Content',
+        }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        labels = {
+            'content': 'Content',
+        }
